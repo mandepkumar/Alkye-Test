651ac9ab70ea5bb745ea5ab2bbec388bdbfa23c0
mandepkumar <162789420+mandepkumar@users.noreply.github.com>
2024-11-24 22:44:03 +0530

    updated the protected route

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,8 +1,9 @@
+import useAuth from "@/hooks/useAuth";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = localStorage.getItem("authToken") !== null;
-  if (isAuthenticated) return children;
+  const { user } = useAuth();
+  if (user != null && user.token != null) return children;
   return <Navigate to="/login" />;
 };
 
